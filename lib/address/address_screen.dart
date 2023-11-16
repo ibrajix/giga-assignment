8f55b39d0f7a109ea63caf6a9946bdd411299b24
Ibrahim <ibrajix@gmail.com>
2023-11-16 17:56:56 +0100

    refactor: add scroll to address screen layout

--- lib/address/address_screen.dart
+++ lib/address/address_screen.dart
@@ -32,11 +32,11 @@ class _AddressScreenState extends State<AddressScreen> with FormMixin {
   final TextEditingController _blockController = TextEditingController();
   final TextEditingController _houseController = TextEditingController();
   final TextEditingController _apartmentController = TextEditingController();
+  bool allFieldsValidated = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
         appBar: gigaAppBar(
           height: 80,
           title: 'Registered Address',
@@ -45,125 +45,129 @@ class _AddressScreenState extends State<AddressScreen> with FormMixin {
               Images.backIcon
             ),
             onPressed: () {
-              Navigator.pop(context);
+             context.pop();
             },
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const LinearProgressIndicator(
-              color: AppColors.gigaYellow,
-              value: 0.5,
-              backgroundColor: AppColors.gigaWhite,
-            ),
-            const SizedBox(height: 20),
-            Container(
-              padding: const EdgeInsets.all(20),
-              child: Form(
-                key: formKey,
-                autovalidateMode: autoValidateMode,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Please enter information as written \non your ID document',
-                      style: TextStyle(
-                          fontSize: 16
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    GigaInputField(
-                      hintText: "Country",
-                      readOnly: true,
-                      validator: Utility.validateString(),
-                      controller: _countryController,
-                      suffixIcon: const Icon(
-                        Icons.search,
-                        color: AppColors.gigaHintColor,
-                      ),
-                      onClickTextField: (){
-                        showCountryPicker(
-                          context: context,
-                          onSelect: (Country country) {
-                            setState(() {
-                              _countryController.text = country.name;
-                            });
-                          },
-                        );
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    GigaInputField(
-                      hintText: "Prefecture",
-                      validator: Utility.validateString(),
-                      controller: _prefectureController,
-                    ),
-                    const SizedBox(height: 10),
-                    GigaInputField(
-                      hintText: "Municipality",
-                      validator: Utility.validateString(),
-                      controller: _municipalityController,
-                    ),
-                    const SizedBox(height: 10),
-                    //ON PURPOSE BECAUSE A TEXT WATCHER ON A SINGLE TEXT FIELD FOR THIS WILL LEAD TO POOR USER EXPERIENCE
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: Form(
+          key: formKey,
+          autovalidateMode: autoValidateMode,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const LinearProgressIndicator(
+                color: AppColors.gigaYellow,
+                value: 0.5,
+                backgroundColor: AppColors.gigaWhite,
+              ),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Text(
+                  'Please enter information as written \non your ID document',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15,
+                  ),
+                  maxLines: 2,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                  child: SingleChildScrollView(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
                       children: [
-                        Expanded(
-                          child: GigaInputField(
-                            hintText: "Subarea",
-                            validator: Utility.validateString(),
-                            controller: _subAreaController,
+                        GigaInputField(
+                          hintText: "Country",
+                          readOnly: true,
+                          validator: Utility.validateString(),
+                          controller: _countryController,
+                          suffixIcon: const Icon(
+                            Icons.search,
+                            color: AppColors.gigaHintColor,
                           ),
+                          onClickTextField: (){
+                            showCountryPicker(
+                              context: context,
+                              onSelect: (Country country) {
+                                setState(() {
+                                  _countryController.text = country.name;
+                                });
+                              },
+                            );
+                          },
                         ),
-                        Expanded(
-                          child: GigaInputField(
-                            hintText: "Block",
-                            validator: Utility.validateString(),
-                            controller: _blockController,
-                          ),
+                        const SizedBox(height: 10),
+                        GigaInputField(
+                          hintText: "Prefecture",
+                          validator: Utility.validateString(),
+                          controller: _prefectureController,
                         ),
-                        Expanded(
-                          child: GigaInputField(
-                            hintText: "House",
-                            validator: Utility.validateString(),
-                            controller: _houseController,
-                          ),
+                        const SizedBox(height: 10),
+                        GigaInputField(
+                          hintText: "Municipality",
+                          validator: Utility.validateString(),
+                          controller: _municipalityController,
+                        ),
+                        const SizedBox(height: 10),
+                        //ON PURPOSE BECAUSE A TEXT WATCHER ON A SINGLE TEXT FIELD FOR THIS WILL LEAD TO POOR USER EXPERIENCE
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Expanded(
+                              child: GigaInputField(
+                                hintText: "Subarea",
+                                validator: Utility.validateString(),
+                                controller: _subAreaController,
+                              ),
+                            ),
+                            Expanded(
+                              child: GigaInputField(
+                                hintText: "Block",
+                                validator: Utility.validateString(),
+                                controller: _blockController,
+                              ),
+                            ),
+                            Expanded(
+                              child: GigaInputField(
+                                hintText: "House",
+                                validator: Utility.validateString(),
+                                controller: _houseController,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        GigaInputField(
+                          hintText: "Apartment, suite or unit",
+                          validator: Utility.validateString(),
+                          controller: _apartmentController,
                         ),
                       ],
                     ),
-                    const SizedBox(height: 10),
-                    GigaInputField(
-                      hintText: "Apartment, suite or unit",
-                      validator: Utility.validateString(),
-                      controller: _apartmentController,
-                    )
-                  ],
-                ),
+                  )
               ),
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Align(
-                alignment: Alignment.center,
-                child: GigaButton(
-                  height: 70,
-                  width: MediaQuery.of(context).size.width,
-                  backgroundColor: AppColors.gigaPurple,
-                    onPressed: () => validate(() {
-                      //save as object
-                      var streetAddressFormat = "${_subAreaController.text} - ${_blockController.text} - ${_houseController.text}";
-                      var address = Address(
-                        country: _countryController.text,
-                        prefecture: _prefectureController.text,
-                        municipality: _municipalityController.text,
-                        streetAddress: streetAddressFormat,
-                        apartment: _apartmentController.text
-                      );
-                      if (kDebugMode) {
-                        print(address.toString());
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: GigaButton(
+                    height: 70,
+                    width: MediaQuery.of(context).size.width,
+                    backgroundColor: AppColors.gigaPurple,
+                      onPressed: () => validate(() {
+                        //save as object
+                        var streetAddressFormat = "${_subAreaController.text} - ${_blockController.text} - ${_houseController.text}";
+                        var address = Address(
+                          country: _countryController.text,
+                          prefecture: _prefectureController.text,
+                          municipality: _municipalityController.text,
+                          streetAddress: streetAddressFormat,
+                          apartment: _apartmentController.text
+                        );
                         showModalBottomSheet<Widget>(
                           isScrollControlled: true,
                           context: context,
@@ -187,15 +191,28 @@ class _AddressScreenState extends State<AddressScreen> with FormMixin {
                             ),
                           ),
                         );
-                      }
-                    }),
-                    text: 'Save'
+                       },
+                      ),
+                      text: 'Next'
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         )
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _countryController.dispose();
+    _prefectureController.dispose();
+    _municipalityController.dispose();
+    _subAreaController.dispose();
+    _blockController.dispose();
+    _houseController.dispose();
+    _apartmentController.dispose();
   }
 
 }
--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,3 +1,4 @@
 class Strings {
   static const notoSans = 'Notosans';
+  //TODO: Put all strings here for ease of translation
 }
